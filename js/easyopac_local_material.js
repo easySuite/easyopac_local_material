98f7a90d6f325e02b0d5a082e99eb87aec239730
Alex <bozeak@gmail.com>
2020-03-06 18:27:04 +0200

    EASYOPAC-1213 - Fixes: part I.

--- js/easyopac_local_material.js
+++ js/easyopac_local_material.js
@@ -7,7 +7,7 @@
   'use strict';
 
   Drupal.behaviors.easyopac_local_material = {
-    attach: function () {
+    attach: function (context, settings) {
       let facets = $('.facetapi-facetapi-checkbox-links');
 
       // Check for click in checkbox, and execute search.
@@ -21,6 +21,52 @@
         if ($(this).not('[target="_blank"]').length) {
           Drupal.TingSearchOverlay();
         }
+      });
+
+      if (settings.facetapi) {
+        for (var index in settings.facetapi.facets) {
+          if (null != settings.facetapi.facets[index].limit) {
+            // Applies soft limit to the list.
+            Drupal.facetapi_local.applyLimit(settings.facetapi.facets[index]);
+          }
+        }
+      }
+    }
+  };
+
+  /**
+   * Class containing functionality for Facet API.
+   */
+  Drupal.facetapi_local = {};
+
+  /**
+   * Applies the soft limit to facets in the block realm.
+   */
+  Drupal.facetapi_local.applyLimit = function (settings) {
+    let ulId = $('ul#' + settings.id);
+    if (settings.limit > 0 && !ulId.hasClass('facetapi-processed')) {
+      // Only process this code once per page load.
+      ulId.addClass('facetapi-processed');
+
+      // Ensures our limit is zero-based, hides facets over the limit.
+      var limit = settings.limit - 1;
+      ulId.find('li:gt(' + limit + ')').hide();
+
+      // Adds "Show more" / "Show fewer" links as appropriate.
+      ulId.filter(function () {
+        return $(this).find('li').length > settings.limit;
+      }).each(function () {
+        $('<a href="#" class="facetapi-limit-link"></a>').text(settings.showMoreText).click(function () {
+          if ($(this).hasClass('open')) {
+            $(this).siblings().find('li:gt(' + limit + ')').slideUp();
+            $(this).removeClass('open').text(settings.showMoreText);
+          }
+          else {
+            $(this).siblings().find('li:gt(' + limit + ')').slideDown();
+            $(this).addClass('open').text(Drupal.t(settings.showFewerText));
+          }
+          return false;
+        }).insertAfter($(this));
       });
     }
   };
